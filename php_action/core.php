37beb76068789f77864d1719f773bb5c73afc620
Asjad Hanif <asjad.hanif970@gmail.com>
2025-06-27 14:35:18 +0500

    URL updated

--- php_action/core.php
+++ php_action/core.php
@@ -6,8 +6,8 @@ require_once 'db_connect.php';
 
 // echo $_SESSION['userId'];
 
-if(!$_SESSION['userId']) {
-	header('location: http://localhost/inventory-management-system/index.php');	
+if(!isset($_SESSION['userId'])) {
+	header('location: http://localhost/IMS/index.php');	
 } 
 
 
